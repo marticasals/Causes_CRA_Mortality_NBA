37e12b139bfb76f8e912096ea2dca6e204273111
Marti Casals <marticasals@gmail.com>
2025-11-26 21:28:19 +0100

    Add files via upload

--- Scripts/MartinezEtAl.R
+++ Scripts/MartinezEtAl.R
@@ -20,9 +20,10 @@ str(nba2019)
 head(nba2019)
 summary(nba2019)
 
-## Subset of African American and White players
-nba2019AAW <- droplevels(subset(nba2019, etni %in% c("Afr.American", "White")))
-summary(nba2019AAW)
+## Subset of players with African and European ancestry
+nba2019AAEA <- droplevels(subset(nba2019, ances %in% c("African ancestry",
+                                                      "European ancestry")))
+summary(nba2019AAEA)
 
 
 ## Table 1
@@ -38,17 +39,17 @@ freq(subset(nba2019, status == 1)$dcause5, plot = FALSE)
 (tabAll <- with(subset(nba2019, status == 1 & dcauseCod != 3), table(dcauseCod)))
 (ptabAll <- prop.table(tabAll))
 rm(tabAll)
-## Subset of African American and White players
-(tabAAW <- with(subset(nba2019AAW, status == 1 & dcauseCod != 3),
-                table(dcauseCod, etni)))
-(ptabAAW <- prop.table(tabAAW, 2))
-rm(tabAAW)
+## Subset of players with African and European ancestry
+(tabAAEA <- with(subset(nba2019AAEA, status == 1 & dcauseCod != 3),
+                table(dcauseCod, ances)))
+(ptabAAEA <- prop.table(tabAAEA, 2))
+rm(tabAAEA)
 
 ## Numbers of missing death causes
 ## -------------------------------
 cod3All <- sum(nba2019$dcauseCod == 3)
-cod3AA <- sum(nba2019AAW$etni == "Afr.American" & nba2019AAW$dcauseCod == 3)
-cod3Wh <- sum(nba2019AAW$etni == "White" & nba2019AAW$dcauseCod == 3)
+cod3AA <- sum(nba2019AAEA$ances == "African ancestry" & nba2019AAEA$dcauseCod == 3)
+cod3EA <- sum(nba2019AAEA$ances == "European ancestry" & nba2019AAEA$dcauseCod == 3)
 
 ## Number of imputations
 ## ---------------------
@@ -60,37 +61,37 @@ M <- 15
 numtimes <- n_distinct(subset(nba2019, status == 1)$ageright)
 Alltimes <- sort(unique(subset(nba2019, status == 1)$ageright))
 
-## African American and White players
-numtimesAAW <- nba2019AAW |>
+## Players with African and European ancestry
+numtimesAAEA <- nba2019AAEA |>
   filter(status == 1) |>
-  group_by(etni) |>
+  group_by(ances) |>
   summarise(n_distinct(ageright)) |>
   as.data.frame()
-AAtimes <- sort(unique(subset(nba2019AAW, status == 1 &
-                                          etni == "Afr.American")$ageright))
-Whtimes <- sort(unique(subset(nba2019AAW, status == 1 &
-                                          etni == "White")$ageright))
+AAtimes <- sort(unique(subset(nba2019AAEA, status == 1 &
+                                           ances == "African ancestry")$ageright))
+EAtimes <- sort(unique(subset(nba2019AAEA, status == 1 &
+                                           ances == "European ancestry")$ageright))
 
 ## Lists for cumulative incidence functions CIFs
 ## ---------------------------------------------
 cifs <- vector("list", 4)
-names(cifs) <- levels(nba2019AAW$dcause5)[c(1, 2, 4, 5)]
+names(cifs) <- levels(nba2019AAEA$dcause5)[c(1, 2, 4, 5)]
 for (i in 1:4) {
   cifs[[i]] <- vector("list", 3)
-  names(cifs[[i]]) <- c("All", levels(nba2019AAW$etni))
+  names(cifs[[i]]) <- c("All", "AA", "EA")
   cifs[[i]][[1]] <- cbind(times = Alltimes,
                           matrix(nrow = numtimes, ncol = M * 2,
                                  dimnames = list(1:numtimes,
                                                  paste0(rep(c("F", "V"), each = M),
                                                         1:M))))
   cifs[[i]][[2]] <- cbind(times = AAtimes,
-                          matrix(nrow = numtimesAAW[1, 2], ncol = M * 2,
-                                 dimnames = list(1:numtimesAAW[1, 2],
+                          matrix(nrow = numtimesAAEA[1, 2], ncol = M * 2,
+                                 dimnames = list(1:numtimesAAEA[1, 2],
                                                  paste0(rep(c("F", "V"), each = M),
                                                         1:M))))
-  cifs[[i]][[3]] <- cbind(times = Whtimes,
-                          matrix(nrow = numtimesAAW[2, 2], ncol = M * 2,
-                                 dimnames = list(1:numtimesAAW[2, 2],
+  cifs[[i]][[3]] <- cbind(times = EAtimes,
+                          matrix(nrow = numtimesAAEA[2, 2], ncol = M * 2,
+                                 dimnames = list(1:numtimesAAEA[2, 2],
                                                  paste0(rep(c("F", "V"), each = M),
                                                         1:M))))
 }
@@ -99,14 +100,14 @@ rm(i)
 ## Lists for Cox models
 ## --------------------
 betas <- vector("list", 4)
-names(betas) <- levels(nba2019AAW$dcause5)[c(1, 2, 4, 5)]
+names(betas) <- levels(nba2019AAEA$dcause5)[c(1, 2, 4, 5)]
 
 ## Lists for parameters (within betas)
 for (i in 1:4){
   betas[[i]] <- vector("list", 2)
   names(betas[[i]]) <- c("Estimate", "Variance")
   betas[[i]][[1]] <- betas[[i]][[2]] <-
-    matrix(ncol = M, nrow = 3, dimnames = list(c("Etni", "Height", "Year"),
+    matrix(ncol = M, nrow = 3, dimnames = list(c("Ancestry", "Height", "Year"),
                                                paste0("Imp.", 1:M)))
 }
 rm(i)
@@ -117,49 +118,49 @@ rm(i)
 set.seed(1893)
 for (m in 1:M) {
   nbaimpdatAll <- nba2019
-  nbaimpdatAAW <- nba2019AAW |>
-    mutate(etni = relevel(etni, ref = "White"))
+  nbaimpdatAAEA <- nba2019AAEA |>
+    mutate(ances = relevel(ances, ref = "European ancestry"))
   ## Imputation of death causes
   nbaimpdatAll[nbaimpdatAll$dcauseCod == 3,
                "dcauseCod"] <- sample(c(1, 2, 4, 5), cod3All, replace = TRUE,
                                       prob = ptabAll)
-  nbaimpdatAAW[nbaimpdatAAW$dcauseCod == 3 & nbaimpdatAAW$etni == "White",
-               "dcauseCod"] <- sample(c(1, 2, 4, 5), cod3Wh, replace = TRUE,
-                                      prob = ptabAAW[, 2])
-  nbaimpdatAAW[nbaimpdatAAW$dcauseCod == 3 & nbaimpdatAAW$etni == "Afr.American",
+  nbaimpdatAAEA[nbaimpdatAAEA$dcauseCod == 3 & nbaimpdatAAEA$ances == "European ancestry",
+               "dcauseCod"] <- sample(c(1, 2, 4, 5), cod3EA, replace = TRUE,
+                                      prob = ptabAAEA[, 2])
+  nbaimpdatAAEA[nbaimpdatAAEA$dcauseCod == 3 & nbaimpdatAAEA$ances == "African ancestry",
                "dcauseCod"] <- sample(c(1, 2, 4, 5), cod3AA, replace = TRUE,
-                                      prob = ptabAAW[, 1])
+                                      prob = ptabAAEA[, 1])
 
   ## Cumulative incidence functions
   AllCIFsAll <- summary(etmCIF(Surv(ageleft, ageright, status) ~ 1, nbaimpdatAll,
                                etype = dcauseCod))
-  AllCIFsAAW <- summary(etmCIF(Surv(ageleft, ageright, status) ~ etni, nbaimpdatAAW,
+  AllCIFsAAEA <- summary(etmCIF(Surv(ageleft, ageright, status) ~ ances, nbaimpdatAAEA,
                                etype = dcauseCod))
   for (d in 1:4) {
     cifs[[d]]$All[, paste0(c("F", "V"), m)] <-
       as.matrix(AllCIFsAll[[1]][[d]][, c("P", "var")])
-    cifs[[d]]$Afr.American[, paste0(c("F", "V"), m)] <-
-      as.matrix(AllCIFsAAW$"etni=Afr.American"[[d]][, c("P", "var")])
-    cifs[[d]]$White[, paste0(c("F", "V"), m)] <-
-      as.matrix(AllCIFsAAW$"etni=White"[[d]][, c("P", "var")])
+    cifs[[d]]$AA[, paste0(c("F", "V"), m)] <-
+      as.matrix(AllCIFsAAEA$"ances=African ancestry"[[d]][, c("P", "var")])
+    cifs[[d]]$EA[, paste0(c("F", "V"), m)] <-
+      as.matrix(AllCIFsAAEA$"ances=European ancestry"[[d]][, c("P", "var")])
   }
-  rm(d, AllCIFsAll, AllCIFsAAW, nbaimpdatAll)
+  rm(d, AllCIFsAll, AllCIFsAAEA, nbaimpdatAll)
 
   ## Cause-specific hazard models
   for (d in 1:4) {
     dc <- ifelse(d <= 2, d, d + 1)
     modCause <- coxph(Surv(ageleft, ageright,
-                           dcauseCod == dc) ~ etni + height + debutyear,
-                      nbaimpdatAAW)
+                           dcauseCod == dc) ~ ances + height + debutyear,
+                      nbaimpdatAAEA)
     coefftable <- summary(modCause)$coef
     ## Parameters estimates
     betas[[d]]$Estimate[, m] <- coefftable[, 1]
     betas[[d]]$Variance[, m] <- coefftable[, 3]^2
     rm(modCause, coefftable, dc)
   }
-  rm(d, nbaimpdatAAW)
+  rm(d, nbaimpdatAAEA)
 }
-rm(m, ptabAll, ptabAAW, cod3All, cod3AA, cod3Wh)
+rm(m, ptabAll, ptabAAEA, cod3All, cod3AA, cod3EA)
 
 
 ## Estimation of cumulative incidences
@@ -168,13 +169,13 @@ rm(m, ptabAll, ptabAAW, cod3All, cod3AA, cod3Wh)
 ## ---------------------------------------------------------
 resultsAll <- data.frame(times = Alltimes, cif1 = NA, cif2 = NA, cif4 = NA, cif5 = NA)
 resultsAA  <- data.frame(times = AAtimes, cif1 = NA, cif2 = NA, cif4 = NA, cif5 = NA)
-resultsWh  <- data.frame(times = Whtimes, cif1 = NA, cif2 = NA, cif4 = NA, cif5 = NA)
+resultsEA  <- data.frame(times = EAtimes, cif1 = NA, cif2 = NA, cif4 = NA, cif5 = NA)
 
 for (d in 1:4) {
   ## CIFs estimates
   resultsAll[, d + 1] <- round(rowMeans(cifs[[d]]$All[, paste0("F", 1:M)]), 4)
-  resultsAA[, d + 1]  <- round(rowMeans(cifs[[d]]$Afr.American[, paste0("F", 1:M)]), 4)
-  resultsWh[, d + 1]  <- round(rowMeans(cifs[[d]]$White[, paste0("F", 1:M)]), 4)
+  resultsAA[, d + 1]  <- round(rowMeans(cifs[[d]]$AA[, paste0("F", 1:M)]), 4)
+  resultsEA[, d + 1]  <- round(rowMeans(cifs[[d]]$EA[, paste0("F", 1:M)]), 4)
 }
 
 ## Table 2
@@ -189,15 +190,15 @@ par(mfrow = c(2, 2), las = 1, font.lab = 4, font.axis = 2, font = 2,
     cex.lab = 1.3, cex.axis = 1.2)
 for (d in 1:4) {
   dn <- ifelse(d <= 2, d, d + 1)
-  with(resultsWh, plot(times, resultsWh[, d + 1], xlab = "Age",
+  with(resultsEA, plot(times, resultsEA[, d + 1], xlab = "Age",
                        ylab = "Cumulative incidence", type = "S", lwd = 3,
                        lty = 2, ylim = c(0, 0.35)))
   with(resultsAA, lines(times, resultsAA[, d + 1], type = "S", lwd = 3, col = 2,
                         lty = 1))
   axis(1, at = seq(20, 95, 5)[-c(5, 9, 13)])
-  legend("topleft", c("African American", "White"), col = 2:1, lty = 1:2,
+  legend("topleft", c("African Ancestry", "European Ancestry"), col = 2:1, lty = 1:2,
          bty = "n", lwd = 3, cex = 1.2)
-  title(paste("Death cause:", levels(nba2019AAW$dcause5)[dn]), cex.main = 1.3)
+  title(paste("Death cause:", levels(nba2019AAEA$dcause5)[dn]), cex.main = 1.3)
 }
 dev.off()
 
@@ -206,8 +207,8 @@ dev.off()
 ## =============================
 ## Data frame with summary statistics for each death cause
 ## -------------------------------------------------------
-results <- data.frame(cause = factor(rep(levels(nba2019AAW$dcause5)[-3], each = 3)),
-                      variables = c("African American", "Height [5 cm]",
+results <- data.frame(cause = factor(rep(levels(nba2019AAEA$dcause5)[-3], each = 3)),
+                      variables = c("African  ancestry", "Height [5 cm]",
                                     "NBA debut [5 years]"),
                       HR = NA, CIlow = NA, CIupp = NA)
 results$cause <- relevel(results$cause, ref = "Neoplasm")
